0ae4852b9e7a86531ea8902a735d58789908ad59
alden76it <aldensirait03@gmail.com>
2024-08-27 21:57:40 +0700

    update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
 // javascript
+
 function gantiNama() {
-    let nama = prompt("Siapa namamu?", "")
-    document.getElementById('nama-user').innerHTML = nama;
+    let nama = prompt("Siapa namamu?", "");
+    document.getElementById('nama-user').innerHTML = nama;     
+    if (nama.length =="") {
+        gantiNama();}  
+
 }
-gantiNama()
+gantiNama();
